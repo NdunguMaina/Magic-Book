261f4d9c4e5823efda1c277e8af82bf3eeff8d54
BLACKSPI <stanleymacharia0001@gmail.com>
2024-06-15 17:07:47 +0300

    latest

--- script.js
+++ script.js
@@ -49,17 +49,17 @@ function createCard(array, index) {
     front.className = 'front';
     front.innerText = array[index];
 
+    const signLanguageImage = document.createElement('img');
+    signLanguageImage.className = 'sign-language-image'; 
+    signLanguageImage.src = signLanguageImages[array[index]];
+    signLanguageImage.alt = 'Sign Language for ' + array[index];
+
+    // Append the sign language image to the card
+    card.appendChild(signLanguageImage);
 
     const back = document.createElement('div');
     back.className = 'back';
     back.innerText = array[(index + 1) % array.length];
-
-    const backImage = document.createElement('img');
-    backImage.className = 'sign-image';
-    backImage.src = `images/${array[(index + 1) % array.length]}.png`; // Ensure the images are named correctly
-
-    front.appendChild(frontImage);
-    back.appendChild(backImage);
 
     innerCard.appendChild(front);
     
@@ -75,6 +75,7 @@ function createCard(array, index) {
 function changeCardContent(button, direction) {
     const card = button.parentElement;
     let array;
+    let newIndex;
 
     if (card.querySelector('.front').innerText.match(/[AEIOU]/i)) {
         array = vowels;
@@ -85,18 +86,22 @@ function changeCardContent(button, direction) {
     }
 
     let index = parseInt(card.getAttribute('data-index'));
-    index = (index + direction + array.length) % array.length;
-    card.setAttribute('data-index', index);
+    newIndex = (index + direction + array.length) % array.length;
+    card.setAttribute('data-index', newIndex);
+    card.querySelector('.front').innerText = array[newIndex];
+    card.querySelector('.back').innerText = array[(newIndex + 1) % array.length];
 
-    const front = card.querySelector('.front');
-    const back = card.querySelector('.back');
-    front.innerText = array[index];
-    back.innerText = array[(index + 1) % array.length];
-
-    const frontImage = front.querySelector('img');
-    const backImage = back.querySelector('img');
-    frontImage.src = `images/${array[index]}.png`;
-    backImage.src = `images/${array[(index + 1) % array.length]}.png`;
+    // Find and update the sign language image
+    const signLanguageImage = card.querySelector('.sign-language-image');
+    if (signLanguageImage) {
+        if (signLanguageImages[array[newIndex]]) {
+            signLanguageImage.src = signLanguageImages[array[newIndex]];
+            signLanguageImage.alt = 'Sign Language for ' + array[newIndex];
+        } else {
+            // Optional: Handle missing images
+            signLanguageImage.src = 'path/to/placeholder.png'; // Path to a generic or placeholder image
+        }
+    }
 
     actionsTaken = true;
 }
@@ -104,8 +109,11 @@ function changeCardContent(button, direction) {
 let actionsTaken = false;
 
 function readWord() {
-   
-
+    // if (!actionsTaken) {
+    //     alert("Please interact with the cards before reading the word.");
+    //     return;
+    // }
+    
     const cards = document.querySelectorAll('.card');
     let word = '';
 
@@ -131,9 +139,9 @@ function readWord() {
     // Speak each letter individually with a delay between each
     async function speakLettersAndWord() {
         for (let i = 0; i < word.length; i++) {
-            await speakLetter(word[i], 100); // 100ms delay between each letter
+            await speakLetter(word[i], 200); // 500ms delay between each letter
         }
-
+        
         // After speaking all letters, speak the whole word
         const utterance = new SpeechSynthesisUtterance(word);
         utterance.voice = speechSynthesis.getVoices().find(voice => voice.name.includes("Google UK English Female"));
@@ -158,3 +166,74 @@ function voiceCard(card) {
     speechSynthesis.speak(utterance);
     actionsTaken = true;
 }
+
+const signLanguageImages = {
+    'A': './images/A.png',
+    'B': './images/B.png',
+    'C': './images/C.png',
+    'D': './images/D.png',
+    'E': './images/E.png',
+    'F': './images/F.png',
+    'G': './images/G.png',
+    'H': './images/H.png',
+    'I': './images/I.png',
+    'J': './images/J.png',
+    'K': './images/K.png',
+    'L': './images/L.png',
+    'M': './images/M.png',
+    'N': './images/N.png',
+    'O': './images/O.png',
+    'P': './images/P.png',
+    'Q': './images/Q.png',
+    'R': './images/R.png',
+    'S': './images/S.png',
+    'T': './images/T.png',
+    'U': './images/U.png',
+    'V': './images/V.png',
+    'W': './images/W.png',
+    'X': './images/X.png',
+    'Y': './images/Y.png',
+    'Z': './images/Z.png',
+    'AI': './images/AI.png',
+    'AW': './images/AW.png',
+    'AY': './images/AY.png',
+    'BL': './images/BL.png',
+    'BR': './images/BR.png',
+    'CE': './images/CE.png',
+    'CH': './images/CH.png',
+    'CK': './images/CK.png',
+    'CR': './images/CR.png',
+    'DE': './images/DE.png',
+    'EA': './images/EA.png',
+    'EE': './images/EE.png',
+    'FL': './images/FL.png',
+    'GR': './images/GR.png',
+    'IE': './images/IE.png',
+    'KE': './images/KE.png',
+    'LL': './images/LL.png',
+    'ME': './images/ME.png',
+    'MP': './images/MP.png',
+    'ND': './images/ND.png',
+    'NG': './images/NG.png',
+    'NE': './images/NE.png',
+    'NK': './images/NK.png',
+    'OA': './images/OA.png',
+    'OI': './images/OI.png',
+    'OO': './images/OO.png',
+    'OY': './images/OY.png',
+    'PL': './images/PL.png',
+    'PR': './images/PR.png',
+    'QU': './images/QU.png',
+    'RE': './images/RE.png',
+    'SH': './images/SH.png',
+    'SL': './images/SL.png',
+    'SP': './images/SP.png',
+    'SS': './images/SS.png',
+    'ST': './images/ST.png',
+    'TE': './images/TE.png',
+    'TH': './images/TH.png',
+    'TR': './images/TR.png',
+    'UE': './images/UE.png',
+    'VE': './images/VE.png',
+    'WH': './images/WH.png'
+};
