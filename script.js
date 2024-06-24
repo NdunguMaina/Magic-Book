93b2982b812645f183c25460524c9345e03297ed
Gichuki-aleki <elgich6067@gmail.com>
2024-06-24 16:11:10 +0300

    colors

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 const consonantsAndSyllablesFirst = [
-    'B', 'C', 'D', 'F', 'G', 'J', 'H', 'K', 'L', 'M', 
-    'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y', 'Z', 
-    'BL', 'BR', 'CR', 'FL', 'GR', 'PL', 'PR', 'SL', 'SP', 'ST', 'TR', 'CH', 'SH', 'TH', 'WH', ''
+    'b', 'c', 'd', 'f', 'g', 'j', 'h', 'k', 'l', 'm', 
+    'n', 'p', 'qu', 'r', 's', 't', 'v', 'w', 'y', 'z', 
+    'bl', 'br', 'cr', 'fl', 'gr', 'pl', 'pr', 'sl', 'sp', 'st', 'tr', 
+    'ch', 'sh', 'th', 'wh', 'ph' ,'cl' , 
+    'fl' , 'gl' , 'fr' , 'sn' ,'sm' , ''
 ];
 
-const vowels = ['A', 'E', 'I', 'O', 'U', 'EA' , 'AI', 'EE' , 'OA', 'OO', 'IE', 'OI', 'OY', 'AY', 'Y', 'UE', 'AW', 'OW', 'A', ''];
+const vowels = ['a', 'e', 'i', 'o', 'u', 'ea', 'ai', 'ee', 'oa', 'oe', 'oo', 'ie', 'oi', 
+    'oy', 'ay', 'y', 'ue', 'ow', 'ou', 'ey', ''];   
 
 const consonantsAndSyllablesLast = [
-    'B', 'CK', 'D', 'G', 'K', 'LL', 'L', 'M', 'N', 'P', 
-    'R', 'S', 'SS', 'T', 'W', 'X', 'CE', 'DE', 'KE', 'ME', 'NE', 
-    'RE', 'TE', 'VE', 'MP', 'ND', 'NK', 'ST', 'NG', 'SH', 'NK', 'TH', 'CH', 'SH', 'SS', ''
+    'b', 'ck', 'd', 'g', 'k', 'll', 'l', 'm', 'n', 'p', 
+    'r', 's', 'ss', 't', 'w', 'x', 'ce', 'de', 'ke', 'me', 'ne', 
+    're', 'te', 've', 'mp', 'nd', 'nk', 'st', 'ng', 'sh', 'nk', 'th', 'ch', 'sh', 
+    'ss', 'pt','ft', 'se', 'tch', 'dge',
+     'ff', 'zz', 'lt', 'lk', 'lf', ''
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
     const cardContainer = document.getElementById('card-container');
 
-    // Create first card with consonants/syllables (blue)
-    cardContainer.appendChild(createCard(consonantsAndSyllablesFirst, 0, 'blue'));
-
-    // Create middle card with vowels (orange)
-    cardContainer.appendChild(createCard(vowels, 0, 'orange'));
-
-    // Create last card with consonants/syllables (blue)
-    cardContainer.appendChild(createCard(consonantsAndSyllablesLast, 0, 'blue'));
+    cardContainer.appendChild(createCard(consonantsAndSyllablesFirst, 0));
+    cardContainer.appendChild(createCard(vowels, 0));
+    cardContainer.appendChild(createCard(consonantsAndSyllablesLast, 0));
 });
 
-function createCard(array, index, colorClass) {
+function createCard(array, index) {
     const card = document.createElement('div');
     card.className = 'card';
     card.setAttribute('data-letter', array[index]);
@@ -35,67 +35,95 @@ function createCard(array, index, colorClass) {
     const upButton = document.createElement('button');
     upButton.className = 'arrow up';
     upButton.innerHTML = '&#9650;';
-    upButton.setAttribute('onclick', 'changeCardContent(this, 1)');
+    upButton.setAttribute('onclick', 'changeCardContent(event, this, 1)');
+    upButton.setAttribute('aria-label', 'Move to previous letter');
 
     const downButton = document.createElement('button');
     downButton.className = 'arrow down';
     downButton.innerHTML = '&#9660;';
-    downButton.setAttribute('onclick', 'changeCardContent(this, -1)');
+    downButton.setAttribute('onclick', 'changeCardContent(event, this, -1)');
+    downButton.setAttribute('aria-label', 'Move to next letter');
 
     const innerCard = document.createElement('div');
     innerCard.className = 'inner-card';
 
     const front = document.createElement('div');
-    front.className = 'front ' + colorClass; // Add color class here
-    front.innerText = array[index];
-
-    const signLanguageImage = document.createElement('img');
-    signLanguageImage.className = 'sign-language-image'; 
-    signLanguageImage.src = signLanguageImages[array[index]];
-    signLanguageImage.alt = 'Sign Language for ' + array[index];
-
-    // Append the sign language image to the card
-    card.appendChild(signLanguageImage);
+    front.className = 'front';
+    front.innerHTML = colorText(array[index]);
+    front.tabIndex = 0;
 
     const back = document.createElement('div');
-    back.className = 'back ' + colorClass; // Add color class here
-    back.innerText = array[(index + 1) % array.length];
+    back.className = 'back';
+    back.innerHTML = colorText(array[(index + 1) % array.length]);
 
     innerCard.appendChild(front);
     innerCard.appendChild(back);
 
     card.appendChild(upButton);
     card.appendChild(innerCard);
+
+    const signLanguageImage = document.createElement('img');
+    signLanguageImage.className = 'sign-language-image'; 
+    signLanguageImage.src = signLanguageImages[array[index]];
+    signLanguageImage.alt = 'Sign Language for ' + array[index];
+
+    card.appendChild(signLanguageImage);
     card.appendChild(downButton);
 
     return card;
 }
 
-function changeCardContent(button, direction) {
-    const card = button.parentElement;
-    let array;
-    let newIndex;
+function colorText(text) {
+    return text.split('').map(char => {
+        return `<span class="${getColorClass(char)}">${char}</span>`;
+    }).join('');
+}
 
-    if (card.querySelector('.front').innerText.match(/[AEIOU]/i)) {
-        array = vowels;
-    } else if (card === card.parentElement.firstChild) {
-        array = consonantsAndSyllablesFirst;
-    } else {
-        array = consonantsAndSyllablesLast;
-    }
+function getColorClass(char) {
+    const vowels = 'aeiou';
+    return vowels.includes(char) ? 'orange' : 'blue';
+}
+
+function changeCardContent(event, button, direction) {
+    event.stopPropagation();
+    const card = button.closest('.card');
+    const cardText = card.querySelector('.front').innerText;
+    const array = getArrayFromCardText(cardText, card);
 
     const currentIndex = parseInt(card.getAttribute('data-index'));
-    newIndex = (currentIndex + direction + array.length) % array.length;
+    const arrayLength = array.length;
+    const newIndex = (currentIndex + direction + arrayLength) % arrayLength;
 
-    card.querySelector('.front').innerText = array[newIndex];
-    card.querySelector('.back').innerText = array[(newIndex + 1) % array.length];
-    card.setAttribute('data-index', newIndex);
-    card.setAttribute('data-letter', array[newIndex]);
+    updateCardContent(card, array, newIndex);
+}
 
-    // Update the sign language image
+function getArrayFromCardText(text, card) {
+    if (text.match(/[aeiouy]/i)) {
+        return vowels;
+    } else {
+        const isFirstCard = card === card.parentNode.firstChild;
+        const isLastCard = card === card.parentNode.lastChild;
+        if (isFirstCard) {
+            return consonantsAndSyllablesFirst;
+        } else if (isLastCard) {
+            return consonantsAndSyllablesLast;
+        } else {
+            return consonantsAndSyllablesLast;
+        }
+    }
+}
+
+function updateCardContent(card, array, index) {
+    const front = card.querySelector('.front');
+    const back = card.querySelector('.back');
+    front.innerHTML = colorText(array[index]);
+    back.innerHTML = colorText(array[(index + 1) % array.length]);
+    card.setAttribute('data-index', index);
+    card.setAttribute('data-letter', array[index]);
+
     const signLanguageImage = card.querySelector('.sign-language-image');
-    signLanguageImage.src = signLanguageImages[array[newIndex]];
-    signLanguageImage.alt = 'Sign Language for ' + array[newIndex];
+    signLanguageImage.src = signLanguageImages[array[index]];
+    signLanguageImage.alt = 'Sign Language for ' + array[index];
 }
 
 function voiceCard(card) {
@@ -107,84 +135,128 @@ function voiceCard(card) {
 function readWord() {
     const cards = document.querySelectorAll('.card');
     let word = '';
+    let utterances = [];
+
     cards.forEach(card => {
-        word += card.getAttribute('data-letter');
+        const letter = card.getAttribute('data-letter');
+        word += letter;
+        const msg = new SpeechSynthesisUtterance(letter);
+        utterances.push(msg);
     });
-    const msg = new SpeechSynthesisUtterance(word);
+
+    const wordUtterance = new SpeechSynthesisUtterance(word);
+    utterances.push(wordUtterance);
+
+    utterances.reduce((promise, utterance) => {
+        return promise.then(() => {
+            return new Promise(resolve => {
+                utterance.onend = resolve;
+                window.speechSynthesis.speak(utterance);
+            });
+        });
+    }, Promise.resolve());
+
+    checkWordDefinition(word);
+}
+
+function checkWordDefinition(word) {
+    const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const definitionContainer = document.getElementById('definition-container');
+            const definitionElement = document.getElementById('definition');
+            const speakButton = document.getElementById('speak-definition');
+
+            if (data.title === "No Definitions Found") {
+                definitionElement.innerText = "This word does not exist, try again.";
+                speakButton.style.display = 'none';
+            } else {
+                const definition = data[0].meanings[0].definitions[0].definition;
+                const synonyms = data[0].meanings[0].definitions[0].synonyms || [];
+                definitionElement.innerText = `Definition: ${definition}\nSynonyms: ${synonyms.join(', ')}`;
+                speakButton.style.display = 'inline';
+                speakButton.setAttribute('data-definition', definition);
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching definition:', error);
+        });
+}
+
+function speakDefinition() {
+    const definition = document.getElementById('speak-definition').getAttribute('data-definition');
+    const msg = new SpeechSynthesisUtterance(definition);
     window.speechSynthesis.speak(msg);
 }
 
 const signLanguageImages = {
-    'A': 'images/a.png',
-    'B': 'images/b.png',
-    'C': 'images/c.png',
-    'D': 'images/d.png',
-    'E': 'images/e.png',
-    'F': 'images/f.png',
-    'G': 'images/g.png',
-    'H': 'images/h.png',
-    'I': 'images/i.png',
-    'J': 'images/j.png',
-    'K': 'images/k.png',
-    'L': 'images/l.png',
-    'M': 'images/m.png',
-    'N': 'images/n.png',
-    'O': 'images/o.png',
-    'P': 'images/p.png',
-    'Q': 'images/q.png',
-    'R': 'images/r.png',
-    'S': 'images/s.png',
-    'T': 'images/t.png',
-    'U': 'images/u.png',
-    'V': 'images/v.png',
-    'W': 'images/w.png',
-    'X': 'images/x.png',
-    'Y': 'images/y.png',
-    'Z': 'images/z.png',
-    'BL': 'images/bl.png',
-    'BR': 'images/br.png',
-    'CR': 'images/cr.png',
-    'FL': 'images/fl.png',
-    'GR': 'images/gr.png',
-    'PL': 'images/pl.png',
-    'PR': 'images/pr.png',
-    'SL': 'images/sl.png',
-    'SP': 'images/sp.png',
-    'ST': 'images/st.png',
-    'TR': 'images/tr.png',
-    'CH': 'images/ch.png',
-    'SH': 'images/sh.png',
-    'TH': 'images/th.png',
-    'WH': 'images/wh.png',
-    'CK': 'images/ck.png',
-    'LL': 'images/ll.png',
-    'SS': 'images/ss.png',
-    'CE': 'images/ce.png',
-    'DE': 'images/de.png',
-    'KE': 'images/ke.png',
-    'ME': 'images/me.png',
-    'NE': 'images/ne.png',
-    'RE': 'images/re.png',
-    'TE': 'images/te.png',
-    'VE': 'images/ve.png',
-    'MP': 'images/mp.png',
-    'ND': 'images/nd.png',
-    'NK': 'images/nk.png',
-    'ST': 'images/st.png',
-    'NG': 'images/ng.png',
-    'EA': 'images/ea.png',
-    'AI': 'images/ai.png',
-    'EE': 'images/ee.png',
-    'OA': 'images/oa.png',
-    'OO': 'images/oo.png',
-    'IE': 'images/ie.png',
-    'OI': 'images/oi.png',
-    'OY': 'images/oy.png',
-    'AY': 'images/ay.png',
-    'UE': 'images/ue.png',
-    'AW': 'images/aw.png',
-    'OW': 'images/ow.png',
-    'UE': 'images/ue.png',
-    'AW': 'images/aw.png',
-    'OW': 'images/ow.png'
+    'a': 'images/A.png',
+    'b': 'images/B.png',
+    'c': 'images/C.png',
+    'd': 'images/D.png',
+    'e': 'images/E.png',
+    'f': 'images/F.png',
+    'g': 'images/G.png',
+    'h': 'images/H.png',
+    'i': 'images/I.png',
+    'j': 'images/J.png',
+    'k': 'images/K.png',
+    'l': 'images/L.png',
+    'm': 'images/M.png',
+    'n': 'images/N.png',
+    'o': 'images/O.png',
+    'p': 'images/P.png',
+    'q': 'images/Q.png',
+    'r': 'images/R.png',
+    's': 'images/S.png',
+    't': 'images/T.png',
+    'u': 'images/U.png',
+    'v': 'images/V.png',
+    'w': 'images/W.png',
+    'x': 'images/X.png',
+    'y': 'images/Y.png',
+    'z': 'images/Z.png',
+    'bl': 'images/BL.png',
+    'br': 'images/BR.png',
+    'cr': 'images/CR.png',
+    'fl': 'images/FL.png',
+    'gr': 'images/GR.png',
+    'pl': 'images/PL.png',
+    'pr': 'images/PR.png',
+    'sl': 'images/SL.png',
+    'sp': 'images/SP.png',
+    'st': 'images/ST.png',
+    'tr': 'images/TR.png',
+    'ch': 'images/CH.png',
+    'sh': 'images/SH.png',
+    'th': 'images/TH.png',
+    'wh': 'images/WH.png',
+    'ea': 'images/EA.png',
+    'ai': 'images/AI.png',
+    'ee': 'images/EE.png',
+    'oa': 'images/OA.png',
+    'oo': 'images/OO.png',
+    'ie': 'images/IE.png',
+    'oi': 'images/OI.png',
+    'oy': 'images/OY.png',
+    'ay': 'images/AY.png',
+    'ue': 'images/UE.png',
+    'aw': 'images/AW.png',
+    'ow': 'images/OW.png',
+    'ce': 'images/CE.png',
+    'de': 'images/DE.png',
+    'ke': 'images/KE.png',
+    'me': 'images/ME.png',
+    'ne': 'images/NE.png',
+    're': 'images/RE.png',
+    'te': 'images/TE.png',
+    've': 'images/VE.png',
+    'mp': 'images/MP.png',
+    'nd': 'images/ND.png',
+    'nk': 'images/NK.png',
+    'ng': 'images/NG.png',
+    'sh': 'images/SH.png',
+    'ss': 'images/SS.png'
 };
